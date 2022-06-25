fd1434fc3127851709bf543fe46bbab95356f9de
zhehuan.quan <quanzhehuan@gmail.com>
2022-06-25 22:10:28 +0200

    version zero

---
+++ app/src/main/java/com/example/github/RecyclerViewAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.github
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.github.databinding.RecyclerviewRowBinding
+
+class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+    var items = ArrayList<RecyclerData>()
+
+    fun setDataList(data :  ArrayList<RecyclerData>) {
+        this.items = data
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = com.example.github.databinding.RecyclerviewRowBinding.inflate(layoutInflater)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    class MyViewHolder(val binding: RecyclerviewRowBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data: RecyclerData) {
+            binding.recyclerData = data
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("loadImage")
+        fun loadImage(thubmImage: ImageView, url: String) {
+            Glide.with(thubmImage)
+                .load(url)
+                .circleCrop()
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+                .fallback(R.drawable.ic_launcher_foreground)
+                .into(thubmImage)
+        }
+
+    }
+
+}
